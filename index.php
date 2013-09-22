0ada5bf886263992d5e9dfd94dfc5b6f98ba5d83
ann adelsberger <adelsbergerann@gmail.com>
2013-09-22 08:42:55 -0400

    add initial code for index and logic

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<style type='text/css'>
+    	body {
+        	background-color:<?=$background_color?>
+        }
+	</style>
+	<?php
+		require_once('logic.php');
+	?>
+</head>
+
+<body>
+	
+	
+</body>
+</html>
--- logic.php
+++ logic.php
@@ -1,1 +1,13 @@
-New practice page
+<?php
+date_default_timezone_set('America/New_York');
+$now = date('G');
+
+if($now <= 6 && $now >=20 ) {
+	$background_color = '#96dff4';
+
+else {
+	$background_color = '#000';
+}
+
+
+?>
